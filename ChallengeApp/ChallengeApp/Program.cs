2ea451b1123936fdaa9279fcfb26fd5499d5b979
tomaszo06 <bakczysaraj388@gmail.com>
2024-01-29 21:41:52 +0100

    dzien5 Tablice

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] grades = new int[365];
+string[] dayOfWeeks = new string[7];
+dayOfWeeks[0] = "poniedziałek";
+dayOfWeeks[1] = "wtorek";
+dayOfWeeks[2] = "sroda";
+dayOfWeeks[3] = "czwartek";
+dayOfWeeks[4] = "piątek";
+dayOfWeeks[5] = "sobota";
+dayOfWeeks[6] = "niedziela";
+Console.WriteLine(dayOfWeeks[3]);
+
+string[] weekdays = { "ponied", "wtore", "środa", "czwartek", "piątek" };
+Console.WriteLine(weekdays[5]);
