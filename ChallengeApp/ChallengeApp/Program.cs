1dc9cd75aa61bba54660aa04d97b9001629f3573
tomaszo06 <bakczysaraj388@gmail.com>
2024-02-01 20:07:45 +0100

    Day 5 Tablice  listy i pętle for foreach

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,13 +1,26 @@
 ﻿int[] grades = new int[365];
-string[] dayOfWeeks = new string[7];
-dayOfWeeks[0] = "poniedziałek";
-dayOfWeeks[1] = "wtorek";
-dayOfWeeks[2] = "sroda";
-dayOfWeeks[3] = "czwartek";
-dayOfWeeks[4] = "piątek";
-dayOfWeeks[5] = "sobota";
-dayOfWeeks[6] = "niedziela";
-Console.WriteLine(dayOfWeeks[3]);
+
+List<string> dayOfWeeks = new List<string>();
+
+dayOfWeeks.Add("poniedziałek");
+dayOfWeeks.Add("wtorek");
+dayOfWeeks.Add("sroda");
+dayOfWeeks.Add("czwartek");
+dayOfWeeks.Add("piątek");
+dayOfWeeks.Add("sobota");
+dayOfWeeks.Add("niedziela");
+//Console.WriteLine(dayOfWeeks[3]);
 
 string[] weekdays = { "ponied", "wtore", "środa", "czwartek", "piątek" };
-Console.WriteLine(weekdays[5]);
+//Console.WriteLine(weekdays[5]);
+
+//  Pętle
+
+for (int i = 0; i < dayOfWeeks.Count; i++)
+    //{
+    //    Console.WriteLine(dayOfWeeks[i]);
+    //}
+    foreach (var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
