aeeecf4cc5260024b1c6225cdfd5d3f08a627362
tomaszo06 <bakczysaraj388@gmail.com>
2024-02-12 22:20:36 +0100

    Day 5 ver.1

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,26 +1,71 @@
-﻿int[] grades = new int[365];
+﻿
+int number = 4566;
 
-List<string> dayOfWeeks = new List<string>();
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-dayOfWeeks.Add("poniedziałek");
-dayOfWeeks.Add("wtorek");
-dayOfWeeks.Add("sroda");
-dayOfWeeks.Add("czwartek");
-dayOfWeeks.Add("piątek");
-dayOfWeeks.Add("sobota");
-dayOfWeeks.Add("niedziela");
-//Console.WriteLine(dayOfWeeks[3]);
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-string[] weekdays = { "ponied", "wtore", "środa", "czwartek", "piątek" };
-//Console.WriteLine(weekdays[5]);
-
-//  Pętle
-
-for (int i = 0; i < dayOfWeeks.Count; i++)
-    //{
-    //    Console.WriteLine(dayOfWeeks[i]);
-    //}
-    foreach (var day in dayOfWeeks)
+foreach (char letter in letters)
 {
-    Console.WriteLine(day);
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    if (letter == '9')
+    {
+        counter9++;
+    }
 }
+
+Console.WriteLine("ile razy występuje 0 =>" + counter0);
+Console.WriteLine("ile razy występuje 1 =>" +  counter1);
+Console.WriteLine("ile razy występuje 2 =>" +  counter2); 
+Console.WriteLine("ile razy występuje 3 =>" + counter3);
+Console.WriteLine("ile razy występuje 4 =>" + counter4);
+Console.WriteLine("ile razy występuje 5 =>" + counter5);
+Console.WriteLine("ile razy występuje 6 =>" + counter6);
+Console.WriteLine("ile razy występuje 7 =>" + counter7);
+Console.WriteLine("ile razy występuje 8 =>" + counter8);
+Console.WriteLine("ile razy występuje 9 =>" + counter9);
